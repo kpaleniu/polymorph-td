b938d35f81e73b82b362af782c5f02adb676b642
Kari Palenius <kari.palenius@gmail.com>
2012-08-13 19:05:58 +0300

    Moved win_main.cpp under src/win32

---
+++ src/win32/win_main.cpp
@@ -0,0 +1,20 @@
+/**
+ * @file win_main.cpp
+ * 
+ * Holds the definition of the main function for Windows OS.
+ */
+
+#include <window.h>
+
+/**
+ * Windows main.
+ */
+int APIENTRY WinMain(HINSTANCE hInstance,
+		HINSTANCE hPrevInstance,
+		LPSTR lpCmdLine,
+		int nShowCmd)
+{
+	// TODO Create window and call main of game.
+
+	return 0;
+}
