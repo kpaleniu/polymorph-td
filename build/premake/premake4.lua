5976e981d088c8d9dc7b5ad0afebcbc22ebce014
Jens Akerblom <akerblom.jens@gmail.com>
2012-09-09 17:08:31 +0300

    Refactored premake script.

--- build/premake/premake4.lua
+++ build/premake/premake4.lua
@@ -1,30 +1,29 @@
+
+newoption 
+{
+	trigger 	= "with-opengl",
+	description = "Uses OpenGL for rendering."
+}
+
+newoption
+{
+	trigger		= "with-boost",
+	description = "Uses Boost libraries."
+}
+
 projPath = "../.."
 
 solution "polymorph-td"
 	configurations { "Debug", "Release" }
 	includedirs { projPath .. "/include/base",
 				  projPath .. "/include/game" }
-	files { projPath .. "/src/game/**.cpp", 
-			projPath .. "/src/base/**.cpp"}
 	objdir ( projPath .. "/obj/" .. _ACTION )
 
 	-- Generate own directory for each target
 	location (projPath .. "/build/" .. _ACTION)
 	targetdir (projPath .. "/bin")
 
-	-- Boost includes and defines
-	links { "boost_thread",
-			"boost_chrono",
-			"boost_system",
-			"opengl32", }
-	defines { "BOOST_THREAD_USE_LIB" }
-	includedirs { os.getenv("BOOST_HOME") .. "/include" }
-
-	configuration "macosx"
-		-- MacPorts default includes
-		includedirs { "/opt/local/include" }
-		libdirs { "/opt/local/lib" }
-
+	-- From 'configurations'.
 	configuration "Debug"
 		flags { "Symbols", "ExtraWarnings" }
 		defines { "_DEBUG" }
@@ -34,33 +33,49 @@ solution "polymorph-td"
 		flags { "Optimize" }
 		defines { "NDEBUG" }
 		libdirs { projPath .. "/lib/Release" }
+	--
 
 	-- Project for the actual game
 	project "PolyMorphTD"
 		kind "WindowedApp"
 		language "C++"
-		includedirs { projPath .. "/include/win32",
-					  projPath .. "/include/opengl",
-					  projPath .. "/include/wgl",
-					  projPath .. "/include/boost" }
-		files { projPath .. "/src/win32/**.cpp",
-				projPath .. "/src/opengl/**.cpp",
-				projPath .. "/src/wgl/**.cpp",
-				projPath .. "/src/boost/**.cpp" }
-		defines { "WIN32_LEAN_AND_MEAN" }
+
+		files { projPath .. "/src/game/**.cpp", 
+				projPath .. "/src/base/**.cpp"}
+
+		configuration "with-opengl"
+			includedirs { projPath .. "/include/opengl" }
+			links { "opengl32" }
+			files { projPath .. "/src/opengl/**.cpp" }
+			
+		configuration "windows"
+			includedirs { projPath .. "/include/win32" }
+			defines { "WIN32_LEAN_AND_MEAN" }
+			files { projPath .. "/src/win32/**.cpp" }
+			
+		configuration {"windows", "with-opengl"}
+			includedirs { projPath .. "/include/wgl" }
+			files { projPath .. "/src/wgl/**.cpp" }
+	
+		configuration "with-boost"
+			includedirs { projPath .. "/include/boost",
+						  os.getenv("BOOST_HOME") .. "/include" } 
+			defines { "BOOST_THREAD_USE_LIB" }
+			links { "boost_thread",
+					"boost_chrono",
+					"boost_system" }
+			files { projPath .. "/src/boost/**.cpp" }
+
+		configuration "macosx"
+			-- MacPorts default includes
+			includedirs { "/opt/local/include" }
+			libdirs { "/opt/local/lib" }
 
 	-- Project for PolyMorphTD unit tests
-	--[[
 	project "PolyMorphTD-testrunner"
 		kind "ConsoleApp"
 		language "C++"
-		files {
-			"../../include/*.hpp",
-			"../../tst/*.cpp"
-		}
-		excludes {
-			"../../src/main.cpp"
-		}
+		files { projPath .. "/test/**.cpp" }
 		links { "cppunit" }
-	--]]
+	
 	
