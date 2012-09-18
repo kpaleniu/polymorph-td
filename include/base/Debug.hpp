fbef36f7210bbc4ab95b50e612f30302dac951a4
Jens Akerblom <akerblom.jens@gmail.com>
2012-09-18 20:31:50 +0300

    Added debug units.

--- include/base/Debug.hpp
+++ include/base/Debug.hpp
@@ -9,17 +9,19 @@
 #include <iostream>
 
 #ifdef _DEBUG
-#	define DEBUG_OUT(msg) std::clog << msg << std::endl;
+#	define DEBUG_OUT(msg) std::clog << msg << std::endl
 #else
 #	define DEBUG_OUT(msg) (void 0)
 #endif
 
 #define DEBUG_OUT_UNIT(unit, msg) \
 	if (unit)\
-	DEBUG_OUT(msg);
+	{DEBUG_OUT(msg);}
 
 #define BUFFER_UNIT 					1
 #define CYCLIC_AUTO_IO_STREAM_UNIT 		1
 #define CYCLIC_AUTO_OUTPUT_STREAM_UNIT	1
+#define THREAD_UNIT						1
+#define SYSTEM_ACTION_QUEUE_UNIT		1
 
 #endif /* DEBUG_HPP_ */
