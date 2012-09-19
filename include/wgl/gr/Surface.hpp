4b596ca741b2f06a7613d4b6bc684f996889c626
ntx <antti.alho@gmail.com>
2012-09-19 21:42:48 +0300

    wrapped wgl/gr/Surface.hpp HDC__ and HGLRC__ pointers inside std::unique_ptr to ensure resource deletion in the event of exceptions; also implemented isActive() and activate()

--- include/wgl/gr/Surface.hpp
+++ include/wgl/gr/Surface.hpp
@@ -6,18 +6,42 @@
 #ifndef SURFACE_HPP_
 #define SURFACE_HPP_
 
+#include "NonCopyable.hpp"
+#include <memory>
+#include <windows.h>
+#include <wingdi.h>
+
+
 namespace sys {
 class Window;
 }
 
-#include "NonCopyable.hpp"
-#include <windows.h>
-#include <wingdi.h>
-
 namespace gr {
+namespace detail {
+
+/**
+ * Custom deleter type to for HDC__ pointers.
+ */
+class dc_deleter {
+	HWND _window;
+public:
+	explicit dc_deleter(HWND window);
+	void operator()(HDC dc);
+};
+
+/**
+ * Custom deleter type to allow HGLRC__ pointers.
+ */
+struct rc_deleter {
+    void operator()(HGLRC rc);
+};
+
+}
+
 
 /**
  *
+ * TODO: Append GetLastError() information to thrown exceptions.
  */
 class Surface : NonCopyable
 {
@@ -25,13 +49,22 @@ public:
 	Surface(sys::Window &win);
 	~Surface();
 
+	/**
+	 * @return true if the underlying rendering context is active in the calling thread.
+	 */
+	bool isActive() const;
+
+	/**
+	 * Activate/deactivate the underlying rendering context for the calling thread.
+	 */
+	void activate(bool activate = true);
+
 	void flipBuffers();
 
 private:
 	sys::Window &_win;
-
-	HGLRC _glHandle;
-	HDC _deviceHandle;
+	std::unique_ptr<HDC__,   detail::dc_deleter> _deviceHandle;
+	std::unique_ptr<HGLRC__, detail::rc_deleter> _glHandle;
 };
 
 }
