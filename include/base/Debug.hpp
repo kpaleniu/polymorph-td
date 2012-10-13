6493022485ad392cf039f863d334b7fb271d0576
Jens Akerblom <akerblom.jens@gmail.com>
2012-10-13 23:07:28 +0300

    Modified DEBUG_OUT to be atomic.

--- include/base/Debug.hpp
+++ include/base/Debug.hpp
@@ -6,10 +6,26 @@
 #ifndef DEBUG_HPP_
 #define DEBUG_HPP_
 
+#include "concurrency/Thread.hpp"
+#include "concurrency/Mutex.hpp"
 #include <iostream>
 
+namespace debug {
+
+static concurrency::Mutex __outMutex;
+
+}
+
 #ifdef _DEBUG
-#	define DEBUG_OUT(msg) std::clog << msg << std::endl
+#	define DEBUG_OUT(msg) \
+{   concurrency::MutexLockGuard __lock(debug::__outMutex); \
+	std::clog 	<< __FILE__ \
+				<< "@" \
+				<< __LINE__ \
+				<< " thread=" \
+				<< concurrency::Thread::getCurrentID() \
+				<< "\n" \
+				<< msg << std::endl; }
 #else
 #	define DEBUG_OUT(msg) (void 0)
 #endif
