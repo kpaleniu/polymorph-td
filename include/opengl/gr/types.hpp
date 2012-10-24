4c7a17e5fd7c08acd7d27d1b838ddd924b1c0f0a
Jens Akerblom <akerblom.jens@gmail.com>
2012-10-24 20:29:21 +0300

    Added OpenGL types to gr/types.hpp.

--- include/opengl/gr/types.hpp
+++ include/opengl/gr/types.hpp
@@ -12,11 +12,21 @@ namespace gr {
 
 typedef GLfloat real;
 
+const GLenum REAL_TYPE = GL_FLOAT;
+
 enum class BufferUsage : GLenum
 {
 	STATIC = GL_STATIC_DRAW,
 	DYNAMIC = GL_DYNAMIC_DRAW,
 	STREAM = GL_STREAM_DRAW,
+};
+
+enum class Primitive : GLenum
+{
+	LINES = GL_LINES,
+	TRIANGLES = GL_TRIANGLES,
+	TRIANGLE_STRIP = GL_TRIANGLE_STRIP,
+	QUADS = GL_QUADS,
 };
 
 }
