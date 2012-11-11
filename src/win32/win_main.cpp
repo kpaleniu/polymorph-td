60c8413a2692ddad69a409496993a4276ba09285
Jens Akerblom <akerblom.jens@gmail.com>
2012-11-11 14:46:06 +0200

    Updated main to show example usage of the game model.

--- src/win32/win_main.cpp
+++ src/win32/win_main.cpp
@@ -7,6 +7,8 @@
 #include <sys/GraphicsSystem.hpp>
 #include <sys/UISystem.hpp>
 
+#include <game/World.hpp>
+
 #include <Debug.hpp>
 
 #include <input/InputSource.hpp>
@@ -78,30 +80,70 @@ int APIENTRY WinMain(HINSTANCE hInstance,
 	});
 
 
-	std::vector<gr::VertexBuffer> buffers;
+	//std::vector<gr::VertexBuffer> buffers;
 
 	uiSystem.graphicsAccess().actionQueue().pushAction(
-	[&buffers](sys::GraphicsSystemRunner& grSys)
+	[&](sys::GraphicsSystemRunner& grSys)
 	{
-		gr::real vecs[] = {-.5f, -.5f,
-						    .5f, -.5f,
-						    .5f,  .5f,
-						   -.5f,  .5f};
+		grSys.renderer().addRenderAction(
+		[](gr::Renderer& renderer) -> bool
+		{
+			renderer.debugDraw().drawLine2D(0.0f, 0.0f, 1.0f, 1.0f);
+			return true;
+		});
 
-		//grSys.renderer().debugDraw().drawLine2D(0.0f, 0.0f, 1.0f, 1.0f);
-		buffers.push_back(
-			grSys.renderer().
-				bufferManager().
-					createVertexBuffer(gr::VertexFormat::V2,
-					                   gr::BufferUsage::STATIC,
-					                   4, vecs));
-
-		//buffers.back().writeVertices(0, 4, vecs);
-
-		buffers.back().draw(gr::Primitive::QUADS);
-		buffers.clear();
 	});
 
+	DEBUG_OUT(TAG, "Creating world");
+	game::World myWorld;
+	text::string_hash enemy1, enemy2, tower1, target, hp, position;
+
+	enemy1 = text::intern("enemy1");
+	enemy2 = text::intern("enemy2");
+	tower1 = text::intern("tower1");
+
+	position = text::intern("position");
+
+	target = text::intern("target");
+	hp = text::intern("hp");
+
+	auto e1 = myWorld.entityManager.addEntity(enemy1);
+	auto e2 = myWorld.entityManager.addEntity(enemy2);
+	auto t1 = myWorld.entityManager.addEntity(tower1);
+
+	e1.addComponent(position).insert({1.5f, 1.2f});
+	e1.addComponent(hp).insert(100);
+
+	e2.addComponent(position).insert({1.1f, 0.2f});
+	e2.addComponent(hp).insert(75);
+
+	t1.addComponent(position).insert({1.0f, 1.0f});
+	t1.addComponent(target).insert(size_t(enemy1));
+
+	DEBUG_OUT(TAG, "Flipping buffers");
+	myWorld.entityManager.flipBuffers();
+
+	while ( uiSystem.getThreadState() != concurrency::Thread::ThreadState::EXITED)
+	{
+		// Should not be needed to reassign each iteration...
+		size_t theTarget = myWorld.entityManager.findEntity(tower1).findReader(target).read().asSize();
+		auto targetHpWriter = myWorld.entityManager.findEntity(theTarget).findWriter(hp);
+		auto targetHpReader = myWorld.entityManager.findEntity(theTarget).findReader(hp);
+		//
+
+		if (targetHpReader.read().asInt() <= 0)
+			break;
+
+		targetHpWriter.write(targetHpReader.read().asInt() - 2);
+
+		DEBUG_OUT(TAG, "HP: %i", targetHpReader.read().asInt());
+
+		myWorld.entityManager.flipBuffers();
+		concurrency::Thread::sleep(sys::TimeDuration::millis(1000));
+	}
+
+
+	uiSystem.interrupt();
 	uiSystem.join();
 
 	DEBUG_OUT(TAG, "UI system is shut down");
