27c412729ccf8fd83b9d2f8a014a5fb83de654b2
Jens Akerblom <akerblom.jens@gmail.com>
2012-12-06 22:08:23 +0200

    Fixed PrivateHandle.
    Macro can only be used if inheriting constructors are available.

--- include/base/ext/PrivateHandle.hpp
+++ include/base/ext/PrivateHandle.hpp
@@ -1,12 +1,40 @@
 /**
  * @file PrivateHandle.hpp
  *
+ * Defines a wrapper that limits the visibility of the object it wraps.
+ *
+ * NOTE, this requires delegating constructors.
  */
 
 #ifndef PRIVATEHANDLE_HPP_
 #define PRIVATEHANDLE_HPP_
 
-#define PRIVATE_HANDLE_T(clazz, friendClazz) \
-	struct {private: clazz value; friend class friendClazz;}
+/**
+ * Base class for the wrapper.
+ */
+template <typename T>
+class PrivateHandle
+{
+protected:
+	PrivateHandle()				: _val() 	{}
+	PrivateHandle(const T& val) : _val(val)	{}
+
+	PrivateHandle& operator=(const T& val)
+	{ _val = val; return *this; }
+
+	operator T()
+	{ return _val; }
+
+private:
+	T _val;
+};
+
+
+// Should mostly be used with typedef.
+// Parameter "friendObject" should be in format "class MyClass" for classes.
+#define NEW_PRIVATE_HANDLE_T(clazz, friendObject) \
+class : public PrivateHandle<clazz> \
+{ using PrivateHandle<clazz>::PrivateHandle; \
+  friend friendObject; }
 
 #endif /* PRIVATEHANDLE_HPP_ */
