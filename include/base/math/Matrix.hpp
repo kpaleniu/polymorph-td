9a511d0e47301fa04b4dff5bc4d023b01e27ddb6
Jens Akerblom <akerblom.jens@gmail.com>
2012-12-08 00:39:07 +0200

    Updated and fixed class Matrix.
    Update includes matrix-matrix multiplication (and hence also matrix-vector multiplication).

--- include/base/math/Matrix.hpp
+++ include/base/math/Matrix.hpp
@@ -22,12 +22,28 @@ public:
 	Matrix();
 	Matrix(S diagonal);
 	Matrix(const std::array<S, Rows * Cols>& data);
+	Matrix(const Matrix<S, Rows, Cols, RowMajor>& mat);
 
 	S* data();
 	const S* data() const;
 
 	S& operator()(index_t row, index_t col);
 	const S& operator()(index_t row, index_t col) const;
+
+	Matrix<S, Rows, Cols, RowMajor> operator*(S scalar) const;
+	Matrix<S, Rows, Cols, RowMajor> operator/(S scalar) const;
+	Matrix<S, Rows, Cols, RowMajor> operator*=(S scalar);
+	Matrix<S, Rows, Cols, RowMajor> operator/=(S scalar);
+
+	template<unsigned int N, bool RM>
+	Matrix<S, Rows, N, RM> operator*(const Matrix<S, Cols, N, RM>& other) const;
+
+private:
+	Matrix(const Eigen::Matrix<S, Rows, Cols, RowMajor ? Eigen::RowMajor : Eigen::ColMajor>& mat);
+
+	// Yes this is legal C++11.
+	template<typename _S, unsigned int _Rows, unsigned int _Cols, bool _RowMajor>
+	friend class Matrix;
 };
 
 }
@@ -40,9 +56,13 @@ namespace math {
 typedef Matrix<real_t, 3, 3> Matrix3x3_r;
 typedef Matrix<real_t, 4, 4> Matrix4x4_r;
 
+typedef Matrix<real_t, 2, 1, false> Vector2_r;
+typedef Matrix<real_t, 3, 1, false> Vector3_r;
+typedef Matrix<real_t, 4, 1, false> Vector4_r;
 
 // Implementation:
 
+// Constructors	START
 template<typename S, unsigned int Rows, unsigned int Cols, bool RowMajor>
 Matrix<S, Rows, Cols, RowMajor>::Matrix()
 :	Eigen::Matrix<S, Rows, Cols, RowMajor ? Eigen::RowMajor : Eigen::ColMajor>()
@@ -62,9 +82,29 @@ template<typename S, unsigned int Rows, unsigned int Cols, bool RowMajor>
 Matrix<S, Rows, Cols, RowMajor>::Matrix(const std::array<S, Rows * Cols>& data)
 :	Eigen::Matrix<S, Rows, Cols, RowMajor ? Eigen::RowMajor : Eigen::ColMajor>()
 {
-	memcpy(Eigen::Matrix<S, Rows, Cols, RowMajor ? Eigen::RowMajor : Eigen::ColMajor>
-				::data(), data.data(), Rows * Cols);
+	// Could be done with memcpy?
+	for (index_t row = 0; row < Rows; ++row)
+	{
+		for (index_t col = 0; col < Cols; ++col)
+		{
+			Eigen::Matrix<S, Rows, Cols, RowMajor ? Eigen::RowMajor : Eigen::ColMajor>
+											::operator()(row, col) = data[(RowMajor ? row : col) * Cols
+											                              + (RowMajor ? col : row)];
+		}
+	}
 }
+
+template<typename S, unsigned int Rows, unsigned int Cols, bool RowMajor>
+Matrix<S, Rows, Cols, RowMajor>::Matrix(const Matrix<S, Rows, Cols, RowMajor>& mat)
+:	Eigen::Matrix<S, Rows, Cols, RowMajor ? Eigen::RowMajor : Eigen::ColMajor>(mat)
+{}
+
+template<typename S, unsigned int Rows, unsigned int Cols, bool RowMajor>
+Matrix<S, Rows, Cols, RowMajor>::Matrix(
+		const Eigen::Matrix<S, Rows, Cols, RowMajor ? Eigen::RowMajor : Eigen::ColMajor>& mat)
+:	Eigen::Matrix<S, Rows, Cols, RowMajor ? Eigen::RowMajor : Eigen::ColMajor>(mat)
+{}
+// Constructors	END
 
 template<typename S, unsigned int Rows, unsigned int Cols, bool RowMajor>
 S* Matrix<S, Rows, Cols, RowMajor>::data()
@@ -92,10 +132,49 @@ const S& Matrix<S, Rows, Cols, RowMajor>::operator()(index_t row, index_t col) c
 			::operator()(row, col);
 }
 
+template<typename S, unsigned int Rows, unsigned int Cols, bool RowMajor>
+Matrix<S, Rows, Cols, RowMajor> Matrix<S, Rows, Cols, RowMajor>::operator*(S scalar) const
+{
+	return Eigen::Matrix<S, Rows, Cols, RowMajor ? Eigen::RowMajor : Eigen::ColMajor>
+			::operator*(scalar);
 }
 
 template<typename S, unsigned int Rows, unsigned int Cols, bool RowMajor>
-inline std::ostream& operator <<(std::ostream& out, const math::Matrix<S, Rows, Cols, RowMajor>& mat)
+Matrix<S, Rows, Cols, RowMajor> Matrix<S, Rows, Cols, RowMajor>::operator/(S scalar) const
+{
+	return Eigen::Matrix<S, Rows, Cols, RowMajor ? Eigen::RowMajor : Eigen::ColMajor>
+			::operator/(scalar);
+}
+
+template<typename S, unsigned int Rows, unsigned int Cols, bool RowMajor>
+Matrix<S, Rows, Cols, RowMajor> Matrix<S, Rows, Cols, RowMajor>::operator*=(S scalar)
+{
+	Eigen::Matrix<S, Rows, Cols, RowMajor ? Eigen::RowMajor : Eigen::ColMajor>
+		::operator*=(scalar);
+	return *this;
+}
+
+template<typename S, unsigned int Rows, unsigned int Cols, bool RowMajor>
+Matrix<S, Rows, Cols, RowMajor> Matrix<S, Rows, Cols, RowMajor>::operator/=(S scalar)
+{
+	Eigen::Matrix<S, Rows, Cols, RowMajor ? Eigen::RowMajor : Eigen::ColMajor>
+		::operator/=(scalar);
+	return *this;
+}
+
+template<typename S, unsigned int Rows, unsigned int Cols, bool RowMajor>
+template<unsigned int N, bool RM>
+Matrix<S, Rows, N, RM> Matrix<S, Rows, Cols, RowMajor>
+	::operator*(const Matrix<S, Cols, N, RM>& other) const
+{
+	return Matrix<S, Rows, N, RM>(Eigen::Matrix<S, Rows, Cols, RowMajor ? Eigen::RowMajor : Eigen::ColMajor>
+											::operator*(other));
+}
+
+}
+
+template<typename S, unsigned int Rows, unsigned int Cols, bool RowMajor>
+inline std::ostream& operator<<(std::ostream& out, const math::Matrix<S, Rows, Cols, RowMajor>& mat)
 {
 	out << "Matrix" << Rows << "x" << Cols << " " << (RowMajor ? "Row major" : "Column major") << std::endl;
 
--- src/test/math/test_Math_main.cpp
+++ src/test/math/test_Math_main.cpp
@@ -11,6 +11,16 @@ int main(int argc, char* argv[])
 	Matrix4x4_r m44(1.0f);
 
 	cout << m44;
+	m44 /= 2.0f;
+	cout << m44;
+	m44 *= 2.0f;
+	cout << m44;
+
+	Vector4_r vec4(array<float, 4>{0,0,0,1});
+	cout << vec4;
+	Vector4_r prod = m44 * vec4;
+	cout << prod;
+
 
 	return 0;
 }
