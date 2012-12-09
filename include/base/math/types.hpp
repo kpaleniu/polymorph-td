76e3e6b397952eab2c11d7fd4a4c593330c2face
Jens Akerblom <akerblom.jens@gmail.com>
2012-12-09 18:17:46 +0200

    Added some math constants.

--- include/base/math/types.hpp
+++ include/base/math/types.hpp
@@ -10,7 +10,21 @@ namespace math {
 
 typedef float real_t;
 
+constexpr real_t PI = 3.141592;
+
+constexpr real_t TWO_PI = PI * real_t(2);
+constexpr real_t HALF_PI = PI / real_t(2);
+constexpr real_t QUARTER_PI = PI / real_t(4);
+
+
+constexpr real_t degToRad(real_t deg)
+{ return deg * (PI / real_t(180)); }
+
+constexpr real_t radToDeg(real_t deg)
+{ return deg * (real_t(180) / PI); }
+
+
 }
 
 
-#endif /* TYPES_HPP_ */
+#endif /* MATH_TYPES_HPP_ */
