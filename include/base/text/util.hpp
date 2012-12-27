0eadf525edae35cde32471980bfc2498007c4610
Jens Akerblom <akerblom.jens@gmail.com>
2012-12-27 19:49:00 +0200

    Added compile-time string hashing.

--- include/base/text/util.hpp
+++ include/base/text/util.hpp
@@ -17,6 +17,51 @@ const std::string& get(string_hash hash);
 
 void clearInterned();
 
+
+// Compile-time constant expressions.
+
+inline constexpr size_t strLen(const char* str)
+{
+	return *str ? 1 + strLen(str + 1) : 0;
+}
+
+inline constexpr unsigned long power(unsigned long base, unsigned int exp)
+{
+	return exp != 0 ? base * power(base, exp - 1) : 1;
+}
+
+namespace detail {
+
+/**
+ * Prime used in hashing algorithm.
+ */
+constexpr unsigned long HASH_PRIME = 31;
+
+/**
+ * Recursive hashing algorithm.
+ *
+ * Same hash algorithm as for Java SE 7 String.hashCode().
+ *   s[0]*31^(n-1) + s[1]*31^(n-2) + ... + s[n-1]
+ *
+ * f(n) = p ^ n, p = HASH_PRIME
+ * s(n) = str[n]
+ *
+ * hash = sum [i = 0 .. len(str)-1] ( f(len(str)-1 - i) + s(i) )
+ *
+ */
+inline constexpr string_hash hash_it(const char* str, size_t i, size_t strSize)
+{
+	return str[i] * power(HASH_PRIME, strSize - 1 - i) + (i == strSize - 1 ? 0 : hash_it(str, i + 1, strSize));
+}
+
+}
+
+
+inline constexpr string_hash hash(const char* str)
+{
+	return detail::hash_it(str, 0, strLen(str));
+}
+
 }
 
 
--- src/base/text/util.cpp
+++ src/base/text/util.cpp
@@ -20,20 +20,7 @@ std::map<string_hash, std::string> interned;
 
 string_hash intern(std::string str)
 {
-	// Same hash algorithm as for Java SE 7 String.hashCode().
-	//   s[0]*31^(n-1) + s[1]*31^(n-2) + ... + s[n-1]
-
-	string_hash rHash = 0;
-	const size_t n = str.length();
-
-	const int prime = 31;
-	int f = 1; // = prime^i
-
-	for (int i = n - 1; i >= 0; --i)
-	{
-		rHash += str[i] * f;
-		f *= prime;
-	}
+	string_hash rHash = hash(str.c_str());
 
 #ifdef _DEBUG
 	if (interned.count(rHash) != 0)
