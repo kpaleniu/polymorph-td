c489d9c4ce8438566c6f61c2c8fff42378b706fb
Jens Akerblom <akerblom.jens@gmail.com>
2013-02-03 22:28:27 +0200

    Updated event test and UIEvents..

--- src/test/ext/test_Event_main.cpp
+++ src/test/ext/test_Event_main.cpp
@@ -4,6 +4,9 @@
 
 using namespace std;
 
+struct E
+{};
+
 struct E1
 {
 	int a;
@@ -24,13 +27,19 @@ struct FuckUp
 
 int main(int argc, char* argv[])
 {
-	EventManager<E1, E2, E3> manager1;
+	EventManagers<E, E1, E2, E3> manager1;
 
-	// auto& fuckUpManager = manager1.as<FuckUp>(); // Invalid cast build error.
+	// auto& fuckUpManager = manager1.with<FuckUp>(); // Invalid cast build error.
 
-	auto& e1Bindings = manager1.as<E1>().actionBindings();
-	auto& e2Bindings = manager1.as<E2>().actionBindings();
-	auto& e3Bindings = manager1.as<E3>().actionBindings();
+	auto& eBindings = manager1.with<E>().actionBindings();
+	auto& e1Bindings = manager1.with<E1>().actionBindings();
+	auto& e2Bindings = manager1.with<E2>().actionBindings();
+	auto& e3Bindings = manager1.with<E3>().actionBindings();
+
+	eBindings.push_back([]
+	{
+		cout << "E" << endl;
+	});
 
 	e1Bindings.push_back([](const E1& e1)
 	{
@@ -45,35 +54,28 @@ int main(int argc, char* argv[])
 		cout << "E3: " << e3.a << " " << e3.b << " " << e3.c << endl;
 	});
 
-	manager1.as<E1>().triggerEvent(E1{1});
-	manager1.as<E2>().triggerEvent(E2{1, 2});
-	manager1.as<E3>().triggerEvent(E3{1, 2, 3});
+	manager1.with<E>().triggerEvent();
+	manager1.with<E1>().triggerEvent(E1{1});
+	manager1.with<E2>().triggerEvent(E2{1, 2});
+	manager1.with<E3>().triggerEvent(E3{1, 2, 3});
 
-	EventManager<E1, E2, E3> manager2;
+	// manager1.with<E>().triggerEvent(E{}); // Build error (expects 0 arguments).
+
+	EventManagers<E, E1, E2, E3> manager2;
 
 	manager2 = manager1;
 
-	manager2.as<E1>().triggerEvent(E1{1});
-	manager2.as<E2>().triggerEvent(E2{1, 2});
-	manager2.as<E3>().triggerEvent(E3{1, 2, 3});
+	manager2.with<E>().triggerEvent();
+	manager2.with<E1>().triggerEvent(E1{1});
+	manager2.with<E2>().triggerEvent(E2{1, 2});
+	manager2.with<E3>().triggerEvent(E3{1, 2, 3});
 
-	auto foo = [=]()
-	{
-		auto manager3 = manager2;
-		manager3.as<E1>().triggerEvent(E1{1});
-		manager3.as<E2>().triggerEvent(E2{1, 2});
-		manager3.as<E3>().triggerEvent(E3{1, 2, 3});
-	};
+	EventManagers<E, E1, E2, E3> manager3(manager2);
 
-	foo();
-
-	EventManager<E1, E2, E3> manager4(manager2);
-
-	cout << "===" << endl;
-
-	manager4.as<E1>().triggerEvent(E1{1});
-	manager4.as<E2>().triggerEvent(E2{1, 2});
-	manager4.as<E3>().triggerEvent(E3{1, 2, 3});
+	manager3.with<E>().triggerEvent();
+	manager3.with<E1>().triggerEvent(E1{1});
+	manager3.with<E2>().triggerEvent(E2{1, 2});
+	manager3.with<E3>().triggerEvent(E3{1, 2, 3});
 
 	return 0;
 }
