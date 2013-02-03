e8fa9d4ed726652c69e0559785a2568ed2ee5464
Jens Akerblom <akerblom.jens@gmail.com>
2013-02-03 22:23:23 +0200

    Refactored EventManager so TemplateUtil no longer is needed.

--- include/base/ext/Event.hpp
+++ include/base/ext/Event.hpp
@@ -3,55 +3,64 @@
 #ifndef EVENT_HPP_
 #define EVENT_HPP_
 
-#include "TemplateUtil.hpp"
-
 #include <functional>
 #include <list>
 
-namespace detail
+namespace detail {
+
+template<typename Event, typename ActionType>
+class ManagerBase
 {
-	struct ManagerType
-	{
-		template<typename Event>
-		class type
-		{
-		public:
-			typedef std::function<void (const Event&)> ActionType;
+public:
+	std::list<ActionType>& actionBindings()
+	{ return _eventActions; }
 
-			void triggerEvent(const Event& event)
-			{
-				for (auto& action : _eventActions)
-					action(event);
-			}
+	const std::list<ActionType>& actionBindings() const
+	{ return _eventActions; }
+protected:
+	std::list<ActionType> _eventActions;
+};
 
-			std::list<ActionType>& actionBindings()
-			{ return _eventActions; }
-
-			const std::list<ActionType>& actionBindings() const
-			{ return _eventActions; }
-
-		private:
-			std::list<ActionType> _eventActions;
-		};
-	};
 }
 
+template<typename Event, bool EmptyParam = std::is_empty<Event>::value>
+class EventManager : public detail::ManagerBase<Event, std::function<void (const Event&)>>
+{
+public:
+	typedef typename std::function<void (const Event&)> ActionType;
+
+	void triggerEvent(const Event& param)
+	{
+		for (auto& action : detail::ManagerBase<Event, ActionType>::_eventActions)
+			action(param);
+	}
+};
+
 template<typename Event>
-using ManagerFor = detail::ManagerType::type<Event>;
+class EventManager<Event, true> : public detail::ManagerBase<Event, std::function<void ()>>
+{
+public:
+	typedef typename std::function<void ()> ActionType;
+
+	void triggerEvent()
+	{
+		for (auto& action : detail::ManagerBase<Event, ActionType>::_eventActions)
+			action();
+	}
+};
 
 template<typename... Events>
-class EventManager : public InheritanceIterator<detail::ManagerType, Events...>
+class EventManagers : public EventManager<Events>...
 {
 public:
 	template<typename Event>
-	ManagerFor<Event>& with()
-	{ return *static_cast<ManagerFor<Event>*>(this); }
+	EventManager<Event>& with()
+	{ return *static_cast<EventManager<Event>*>(this); }
 
 	template<typename Event>
-	const ManagerFor<Event>& with() const
-	{ return *static_cast<const ManagerFor<Event>*>(this); }
+	const EventManager<Event>& with() const
+	{ return *static_cast<const EventManager<Event>*>(this); }
 };
-
 
 
 #endif
