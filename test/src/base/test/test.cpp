b3f5992417e3da5278e206750f0bae9eee552a92
Kipt <akerblom.jens@gmail.com>
2013-05-18 14:43:57 +0300

    Made test run in order they're added.

--- test/src/base/test/test.cpp
+++ test/src/base/test/test.cpp
@@ -1,19 +1,19 @@
 #include "test/test.hpp"
 
-#include <map>
+#include <vector>
 #include <iostream>
 
 namespace test {
 
 namespace {
 
-std::map<std::string, Test> s_tests;
+std::vector<std::pair<std::string, Test>> s_tests;
 
 }
 
 void addTest(const std::string& testName, const Test& test)
 {
-	s_tests[testName] = test;
+	s_tests.push_back(std::make_pair(testName, test));
 }
 
 int runTests()
@@ -49,6 +49,9 @@ int runTests()
 		}
 		std::cout << std::endl;
 	}
+
+	std::cout << std::endl;
+	std::cout << "Success: " << (s_tests.size() - i) << '/' << s_tests.size() << std::endl;
 
 	return i;
 }
