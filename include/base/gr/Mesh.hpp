bacca11b8dee2640afc5e541fd3188d1b57273ba
akerblom.jens@gmail.com <akerblom.jens@gmail.com>
2013-06-05 23:32:13 +0300

    Added Mesh and Mesh::SubMesh class.
    A mesh is a collection of vertices with surface properties.
    A sub-mesh is a subset of a mesh with a particular surface property.

---
+++ include/base/gr/Mesh.hpp
@@ -0,0 +1,74 @@
+#ifndef GR_MESH_HPP_
+#define GR_MESH_HPP_
+
+#include "gr/VertexList.hpp"
+// #include "gr/TextureLoader.hpp"
+#include "gr/types.hpp"
+
+#include "gr/Renderer.hpp"
+
+#include <NonCopyable.hpp>
+
+#include <vector>
+
+namespace gr {
+
+
+class Mesh : NonCopyable
+{
+public:
+	class SubMesh : NonCopyable
+	{
+	public:
+		friend class Mesh;
+
+		void render(Renderer& renderer) const;
+		void render(Renderer& renderer, const Transform& transform) const;
+
+		SubMesh(Mesh& parent, 
+				// TextureLoader::TextureHandle texHandle,
+				std::vector<index_t>&& indices);
+
+		// Note, should only be used when parent doesn't change.
+		SubMesh(SubMesh&& other);
+
+	private:
+		Mesh& _parent;
+
+		// TextureLoader::TextureHandle _texHandle;
+
+		std::vector<index_t> _indices;
+	};
+
+	Mesh(VertexList&& vertices, Primitive shape);
+
+	Mesh(Mesh&& other);
+
+	/* TODO If needed
+	typedef std::vector<SubMesh>::iterator sub_mesh_it;
+
+	sub_mesh_it begin();
+	sub_mesh_it end();
+
+	const sub_mesh_it begin() const;
+	const sub_mesh_it end() const;
+	*/
+
+	void addSubMesh(// TextureLoader::TextureHandle texture,
+					std::vector<index_t>&& indices);
+
+	void render(Renderer& renderer) const;
+
+	void render(Renderer& renderer, const Transform& modelTransform) const;
+
+private:
+	VertexList _vertexList;
+	std::vector<SubMesh> _subMeshes;
+
+	Primitive _shape;
+};
+
+
+}
+
+#endif
---
+++ src/base/gr/Mesh.cpp
@@ -0,0 +1,157 @@
+#include "gr/Mesh.hpp"
+
+
+namespace gr {
+
+
+// Mesh::SubMesh
+
+Mesh::SubMesh::SubMesh(Mesh& parent, 
+					   // TextureLoader::TextureHandle texHandle,
+					   std::vector<index_t>&& indices)
+:	_parent(parent),
+	// _texHandle(texHandle),
+	_indices(std::move(indices))
+{
+}
+
+Mesh::SubMesh::SubMesh(SubMesh&& other)
+:	_parent(other._parent),
+	// _texHandle(other._texHandle),
+	_indices(std::move(other._indices))
+{
+}
+
+void Mesh::SubMesh::render(Renderer& renderer) const
+{
+	auto& vertexWriter = 
+		renderer.renderPassManager().vertexWriter(_parent._vertexList.format, 
+												  _parent._shape,
+												  /*_texHandle*/ nullptr,
+												  nullptr);
+
+	index_t startIndex = vertexWriter.getVertexCount();
+	const VertexFormatData& fmtData = getVertexFormatData(_parent._vertexList.format);
+
+	for (index_t index : _indices)
+	{
+		{
+			const real_t* vertices = _parent._vertexList.vertexData(index);
+			for (unsigned int i = 0; i < fmtData.vertDim; ++i)
+				vertexWriter.vertices() << vertices[i];
+		}
+
+		{
+			const real_t* colors = _parent._vertexList.colorData(index);
+			for (unsigned int i = 0; i < fmtData.colorDim; ++i)
+				vertexWriter.colors() << colors[i];
+		}
+
+		{
+			const real_t* texCoords = _parent._vertexList.texCoordData(index);
+			for (unsigned int i = 0; i < fmtData.textDim; ++i)
+				vertexWriter.texCoords() << texCoords[i];
+		}
+
+		vertexWriter.indices() << (startIndex + index);
+	}
+}
+
+void Mesh::SubMesh::render(Renderer& renderer, const Transform& transform) const
+{
+	auto& vertexWriter = 
+		renderer.renderPassManager().vertexWriter(_parent._vertexList.format, 
+												  _parent._shape,
+												  /*_texHandle*/ nullptr,
+												  nullptr);
+
+	index_t startIndex = vertexWriter.getVertexCount();
+	const VertexFormatData& fmtData = getVertexFormatData(_parent._vertexList.format);
+
+	for (index_t index : _indices)
+	{
+		{
+			real_t* vertices = _parent._vertexList.vertexData(index);
+
+			switch (fmtData.vertDim)
+			{
+				case 2:
+				{
+					MapVector2_r mv2(vertices);
+					auto transformedMV2 = transform * mv2;
+
+					vertexWriter.vertices() << transformedMV2[0] 
+											<< transformedMV2[1];
+					break;
+				} 
+
+				case 3:
+				{
+					MapVector2_r mv3(vertices);
+					auto transformedMV3 = transform * mv3;
+
+					vertexWriter.vertices() << transformedMV3[0] 
+											<< transformedMV3[1]
+											<< transformedMV3[2];
+					break;
+				}
+			}
+		}
+
+		{
+			const real_t* colors = _parent._vertexList.colorData(index);
+			for (unsigned int i = 0; i < fmtData.colorDim; ++i)
+				vertexWriter.colors() << colors[i];
+		}
+
+		{
+			const real_t* texCoords = _parent._vertexList.texCoordData(index);
+			for (unsigned int i = 0; i < fmtData.textDim; ++i)
+				vertexWriter.texCoords() << texCoords[i];
+		}
+
+		vertexWriter.indices() << (startIndex + index);
+	}
+}
+
+// Mesh
+
+Mesh::Mesh(VertexList&& vertices, Primitive shape)
+:	_vertexList(std::move(vertices)),
+	_subMeshes(),
+	_shape(shape)
+{
+}
+
+Mesh::Mesh(Mesh&& other)
+:	_vertexList(std::move(other._vertexList)),
+	_subMeshes(),
+	_shape(other._shape)
+{
+	for (SubMesh& subMesh : other._subMeshes)
+	{
+		_subMeshes.push_back(SubMesh(*this, /*texture, */std::move(subMesh._indices)));
+	}
+
+	other._subMeshes.clear();
+}
+
+void Mesh::addSubMesh(// TextureLoader::TextureHandle texture,
+					  std::vector<index_t>&& indices)
+{
+	_subMeshes.push_back(SubMesh(*this, /*texture, */std::move(indices)));
+}
+
+void Mesh::render(Renderer& renderer) const
+{
+	for (const auto& subMesh : _subMeshes)
+		subMesh.render(renderer);
+}
+
+void Mesh::render(Renderer& renderer, const Transform& transform) const
+{
+	for (const auto& subMesh : _subMeshes)
+		subMesh.render(renderer, transform);
+}
+
+}
