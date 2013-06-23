d876e87e1d95f538d030784aaec98943120da3fc
akerblom.jens@gmail.com <akerblom.jens@gmail.com>
2013-06-23 13:42:27 +0300

    Refactored sys test to use NTSystem.

--- test/src/win32/test_sys/winmain.cpp
+++ test/src/win32/test_sys/winmain.cpp
@@ -3,6 +3,25 @@
 #include <sys/UISystem.hpp>
 #include <text/util.hpp>
 
+#include <sys/NTSystem.hpp>
+
+const text::string_hash SWAP_VSYNC_HASH = text::hash("SWAP_VSYNC");
+const text::string_hash TEST_MODEL_HASH = text::hash("TEST_MODEL");
+const text::string_hash MOVE_MODEL_HASH = text::hash("MOVE_MODEL");
+
+const text::string_hash APP_EXIT_HASH = text::hash("APP_EXIT");
+const text::string_hash APP_HIDE_HASH = text::hash("APP_HIDE");
+
+const text::string_hash APP_POINTER_DOWN_HASH = text::hash("APP_POINTER_DOWN");
+const text::string_hash APP_POINTER_DRAG_HASH = text::hash("APP_POINTER_DRAG");
+const text::string_hash APP_POINTER_MOVE_HASH = text::hash("APP_POINTER_MOVE");
+const text::string_hash APP_POINTER_UP_HASH	  = text::hash("APP_POINTER_UP");
+const text::string_hash APP_POINTER_ZOOM	  = text::hash("APP_POINTER_ZOOM");
+
+const text::string_hash TEST_MESH_HASH = text::hash("TEST_MESH");
+
+const char* EVENT_TAG = "EVENT_TAG";
+
 gr::Mesh createTestQuad()
 {
 	std::vector<gr::real_t> vertices;
@@ -33,6 +52,122 @@ gr::Mesh createTestQuad()
 	return quadMesh;
 }
 
+class TestRunner
+{
+public:
+	struct ConstructionArgs
+	{
+		polymorph::sys::UISystem&		uiSys;
+		polymorph::sys::GraphicsSystem& grSys;
+	};
+
+	TestRunner(ConstructionArgs&& args, polymorph::sys::NTSystem<TestRunner>& system)
+	:	_uiSys(args.uiSys), 
+		_grSys(args.grSys),
+		_meshManager(),
+		_system(system)
+	{
+		_uiSys.waitForStartup();
+		addUIListeners();
+
+		_grSys.waitForStartup();
+		addTestModelToScene();
+	}
+
+	TestRunner(TestRunner&& other)
+	:	_uiSys(other._uiSys), 
+		_grSys(other._grSys),
+		_meshManager(std::move(other._meshManager)),
+		_system(other._system)
+	{
+	}
+
+
+	~TestRunner()
+	{
+		auto transaction = _grSys.sceneMutator();
+		transaction.scene.removeModel(TEST_MODEL_HASH);
+	}
+
+	bool update()
+	{
+		return true;
+	}
+
+private:
+	void addTestModelToScene()
+	{
+		auto meshHandle = _meshManager.addMesh(TEST_MESH_HASH, std::move(createTestQuad()));
+
+		std::vector<gr::Model::ModelMesh> meshes;
+		gr::Model::ModelMesh modelMesh = {gr::Transform(), meshHandle};
+
+		meshes.push_back(modelMesh);
+
+		gr::Model testModel(std::move(meshes));
+
+		{
+			auto transaction = _grSys.sceneMutator();
+			transaction.scene.addModel(TEST_MODEL_HASH, std::move(testModel));
+		}
+	}
+
+	void addUIListeners()
+	{
+		_uiSys.addEventAction<polymorph::sys::event::Quit>
+		(
+			APP_EXIT_HASH,
+			[this] 
+			{ 
+				_system.actionQueue().pushAction
+				(
+					[](TestRunner&)
+					{
+						polymorph::concurrency::Thread::interruptCurrent();
+					}
+				);
+			}
+		);
+
+		_uiSys.addEventAction<polymorph::sys::event::Hide>
+		(
+			APP_HIDE_HASH,
+			[&] { DEBUG_OUT(EVENT_TAG, "event::Hide"); }
+		);
+
+		_uiSys.addEventAction<polymorph::sys::event::PointerDrag>
+		(
+			APP_POINTER_DRAG_HASH,
+			[this](const polymorph::sys::event::PointerDrag& arg)
+			{
+				_system.actionQueue().pushAction
+				(
+					[arg](TestRunner& thiz)
+					{
+						auto transaction = thiz._grSys.sceneMutator();
+						auto& model = transaction.scene.model(TEST_MODEL_HASH);
+
+						DEBUG_OUT("MOVE_MODEL", "This is a drag! (%i %i)", arg.dx, arg.dy);
+			
+						model.transform() *= gr::Transform::eulerAngle(gr::real_t(arg.dy) * 0.001f);
+						model.transform() *= gr::Transform::translate(gr::real_t(arg.dx) * 0.001f, 0.0f);
+						
+					}
+				);
+			}
+		);
+
+	}
+
+private:
+	polymorph::sys::UISystem&		_uiSys;
+	polymorph::sys::GraphicsSystem& _grSys;
+
+	gr::MeshManager _meshManager;
+
+	polymorph::sys::NTSystem<TestRunner>& _system;
+};
+
 
 int APIENTRY WinMain(HINSTANCE hInstance,
                      HINSTANCE, LPSTR , int)
@@ -48,159 +183,17 @@ int APIENTRY WinMain(HINSTANCE hInstance,
 	polymorph::sys::UISystem uiSys(std::move(winCtorData));
 	uiSys.start();
 
-	//
-	gr::MeshManager meshManager;
-
-	text::string_hash meshId = text::hash("TEST_MESH");
-	auto meshHandle = meshManager.addMesh(meshId, std::move(createTestQuad()));
-
-	std::vector<gr::Model::ModelMesh> meshes;
-	gr::Model::ModelMesh modelMesh = {gr::Transform(), meshHandle};
-
-	meshes.push_back(modelMesh);
-
-	gr::Model testModel(std::move(meshes));
-	//
-
-	std::atomic<bool> lastSetVsync = true;
-
-	const text::string_hash SWAP_VSYNC_HASH = text::hash("SWAP_VSYNC");
-
-	const text::string_hash TEST_MODEL_HASH = text::hash("TEST_MODEL");
-	const text::string_hash MOVE_MODEL_HASH = text::hash("MOVE_MODEL");
-
-	const text::string_hash APP_EXIT_HASH = text::hash("APP_EXIT");
-	const text::string_hash APP_HIDE_HASH = text::hash("APP_HIDE");
-
-	const text::string_hash APP_POINTER_DOWN_HASH = text::hash("APP_POINTER_DOWN");
-	const text::string_hash APP_POINTER_DRAG_HASH = text::hash("APP_POINTER_DRAG");
-	const text::string_hash APP_POINTER_MOVE_HASH = text::hash("APP_POINTER_MOVE");
-	const text::string_hash APP_POINTER_UP_HASH	  = text::hash("APP_POINTER_UP");
-	const text::string_hash APP_POINTER_ZOOM	  = text::hash("APP_POINTER_ZOOM");
-
-	const char* EVENT_TAG = "EVENT_TAG";
-
-	std::atomic<bool> quitFlag = false;
-
-	uiSys.addEventAction<polymorph::sys::event::Quit>
-	(
-		APP_EXIT_HASH,
-		[&] { quitFlag = true; }
-	);
-
-	uiSys.addEventAction<polymorph::sys::event::Hide>
-	(
-		APP_HIDE_HASH,
-		[&] { DEBUG_OUT(EVENT_TAG, "event::Hide"); }
-	);
-
-	uiSys.addEventAction<polymorph::sys::event::PointerDown>
-	(
-		APP_POINTER_DOWN_HASH,
-		[&](const polymorph::sys::event::PointerDown& arg)
-		{
-			const gr::Vector2_r v2 = static_cast<const gr::Vector2_r&>(arg.pointerPos);
-			DEBUG_OUT(EVENT_TAG, 
-					  "event::PointerDown(%i, %i, {%f, %f})", 
-					  arg.id, arg.button, 
-					  v2[0], 
-					  v2[1]);
-		}
-	);
-
-	uiSys.addEventAction<polymorph::sys::event::PointerDrag>
-	(
-		APP_POINTER_DRAG_HASH,
-		[&](const polymorph::sys::event::PointerDrag& arg)
-		{
-			DEBUG_OUT(EVENT_TAG, 
-					  "event::PointerDrag(%i, %i, %i)",
-					  arg.id, arg.dx, arg.dy);
-		}
-	);
-
-	uiSys.addEventAction<polymorph::sys::event::PointerMove>
-	(
-		APP_POINTER_MOVE_HASH,
-		[&](const polymorph::sys::event::PointerMove& arg)
-		{
-			DEBUG_OUT(EVENT_TAG,
-					  "event::PointerMove(%i, %i, %i)",
-					  arg.id, arg.dx, arg.dy);
-		}
-	);
-
-	uiSys.addEventAction<polymorph::sys::event::PointerUp>
-	(
-		APP_POINTER_UP_HASH,
-		[&](const polymorph::sys::event::PointerUp& arg)
-		{
-			const gr::Vector2_r v2 = static_cast<const gr::Vector2_r&>(arg.pointerPos);
-			DEBUG_OUT(EVENT_TAG,
-					  "event::PointerUp(%i, %i, {%f, %f})",
-					  arg.id, arg.button, v2[0], v2[1]);
-		}
-	);
-
-	uiSys.addEventAction<polymorph::sys::event::PointerZoom>
-	(
-		APP_POINTER_ZOOM,
-		[&](const polymorph::sys::event::PointerZoom& arg)
-		{
-			DEBUG_OUT(EVENT_TAG, "event::PointerZoom(%i)", arg.dz);
-		}
-	);
-
-	uiSys.waitForStartup();
-
 	auto& grSys = uiSys.waitForGraphicsSystem();
 
+	TestRunner::ConstructionArgs ctorArgs = {uiSys, grSys};
+
 	{
-		auto transaction = grSys.sceneMutator();
-		transaction.scene.addModel(TEST_MODEL_HASH, std::move(testModel));
+		polymorph::sys::NTSystem<TestRunner> 
+			mainSystem(TimeDuration::millis(10), 256, std::move(ctorArgs));
+
+		mainSystem.enter();
 	}
-
-	uiSys.addEventAction<polymorph::sys::event::PointerDrag>
-	(
-		MOVE_MODEL_HASH,
-		[&](const polymorph::sys::event::PointerDrag& arg)
-		{
-			auto transaction = grSys.sceneMutator();
-			auto& model = transaction.scene.model(TEST_MODEL_HASH);
-
-			DEBUG_OUT("MOVE_MODEL", "This is a drag!");
-			
-			model.transform() *= gr::Transform::translate(gr::real_t(arg.dx) * 0.001f, 0.0f);
-			model.transform() *= gr::Transform::eulerAngle(gr::real_t(arg.dy) * 0.001f);
-		}
-	);
-
-	uiSys.addEventAction<polymorph::sys::event::PointerUp>
-	(
-		SWAP_VSYNC_HASH,
-		[&](const polymorph::sys::event::PointerUp& arg)
-		{
-			DEBUG_OUT("SWAP_VSYNC", "Toggling VSync.");
-			grSys.actionQueue().pushAction
-			(
-				[&](polymorph::sys::GraphicsSystemRunner& runner)
-				{
-					runner.surface().setVSync(!lastSetVsync);
-					lastSetVsync = !lastSetVsync;
-				}
-			);
-		}
-	);
-
-
-	while ( !quitFlag )
-	{
-
-		polymorph::concurrency::Thread::sleep(TimeDuration::millis(60));
-	}
-
-	uiSys.removeEventAction<polymorph::sys::event::Quit>(APP_EXIT_HASH);
-
+	
 	uiSys.interrupt();
 	uiSys.join();
 
