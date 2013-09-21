b9eeddad84659881db80bb0b5f0b05acfbe6bfe4
Kari Palenius <kari.palenius@gmail.com>
2013-09-21 13:27:58 +0300

    Added boilerplate headers for cocoa implementations.

---
+++ include/cocoa/input/PointerListener.hpp
@@ -0,0 +1,29 @@
+/**
+ * @file PointerListener.hpp
+ *
+ */
+
+#ifndef POINTERLISTENER_HPP_
+#define POINTERLISTENER_HPP_
+
+namespace polymorph { namespace input {
+
+typedef unsigned short pointer_button;
+typedef int pointer_id;
+
+class PointerListener
+{
+public:
+	virtual ~PointerListener() {}
+
+	virtual void onPointerDown(pointer_id id, pointer_button button, long x, long y) = 0;
+	virtual void onPointerMove(pointer_id id, int dx, int dy) = 0;
+	virtual void onPointerDrag(pointer_id id, int dx, int dy) = 0;
+	virtual void onPointerUp(pointer_id id, pointer_button button, long x, long y) = 0;
+	virtual void onPointerZoom(int dz) = 0;
+};
+
+} }
+
+
+#endif /* POINTERLISTENER_HPP_ */
